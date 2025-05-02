1a917f3a1d5c8404bfebffaa761183e7a1774834
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-05-01 23:51:58 -0300

    Seccion 6: Interactuando con elementos en una página web - 34. El click: Nuestro fiel amigo

---
+++ tests/AutomationSandbox.spec.ts
@@ -0,0 +1,24 @@
+import { test, Browser, Page, expect } from '@playwright/test';
+
+(async () => {
+    let browser: Browser;
+    let page: Page;
+
+    test.describe('Acciones en el Automation Sandbox', () => {
+        test('Click en Botón ID Dinámico', async ({ page }) => {
+
+            await test.step('Dado que navego al Sandbox de Automation de Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');
+            })
+
+            await test.step('Puedo hacer click en el botón con ID dinámico', async () => {
+                const botonIDDinamico = page.getByRole('button', { name: 'Hacé click para generar un ID dinámico y mostrar el elemento oculto' });
+                await botonIDDinamico.click({ force: true });
+                await botonIDDinamico.dblclick();
+                await botonIDDinamico.click({ button: 'right' });
+                await botonIDDinamico.click({ modifiers:['Shift'] });
+                await botonIDDinamico.hover();
+            })
+        })
+    })
+})();
