c12f53712e38395eab0629d927a95884b62ed1c7
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-06-08 02:33:46 -0300

    79. Mejores practicas +test secuencial (no recomendado)

---
+++ tests/ejemplosecuencial.spec.ts
@@ -0,0 +1,23 @@
+import { test, type Page } from '@playwright/test';
+
+// Anotamos todos los tests bajo Serial
+test.describe.configure({ mode: 'serial' });
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+    await page.close();
+});
+
+// si a este le agrego test.skip() se va a romper el que viene
+test('Si no corro primero exploto', async () => {
+    await page.goto('https://playwright.dev/');
+});
+
+test('Si no corro segundo exploto', async () => {
+    await page.getByText('Get Started').click();
+});
